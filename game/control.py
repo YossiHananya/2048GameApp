053064a9c835285a05d70aa3535b111201cc0b71
Yossi Hananya <yossiha93@gmail.com>
2023-04-03 02:19:05 +0300

    feat: add quit option

--- game/control.py
+++ game/control.py
@@ -1,7 +1,13 @@
+from enum import Enum
 from .model.enums import Move
 
+class Flow(Enum):
+  Pause = 1
+  Quit = 2
+  
 
 Arrows_map = {
+  Flow.Quit: ['q', 'Q'],
   Move.Up: ['w', 'W'],
   Move.Down: ['s', 'S'],
   Move.Left: ['a', 'A'],
@@ -17,7 +23,7 @@ class GameControl:
   def start(self):
       while not self.model.is_done:
         self.view.show_game_board(board=self.model.board)
-        allowed_inputs=[] 
+        allowed_inputs = Arrows_map[Flow.Quit]
         
         for move in self.model.valid_moves:
           allowed_inputs.extend(Arrows_map[move])
@@ -25,6 +31,9 @@ class GameControl:
         next_move_input = self.view.get_next_move_from_user(
                       valid_moves=allowed_inputs
         )
+
+        if next_move_input in Arrows_map[Flow.Quit]:
+          break
 
         selected_move = None
         for move in Move:
@@ -34,3 +43,4 @@ class GameControl:
         results = self.model.move(move=selected_move)
         
       self.view.show_game_results(self.model.results)
+      self.view.display_exit()

1e875db38180886bde9e56f784b8ca7dd2265f3c
Yossi Hananya <yossiha93@gmail.com>
2023-04-04 18:20:19 +0300

    refactor: expand tests + add gitignore

--- game/model/tests/test_board.py
+++ game/model/tests/test_board.py
@@ -4,6 +4,8 @@ from game.model.board import Board
 
 
 @pytest.mark.parametrize(
+    ("table", "move", "expected"),
+    [
         (
             [
                 [4, 0, 0 , 0],
@@ -18,11 +20,29 @@ from game.model.board import Board
                 [0, 0, 0 , 0],
                 [0, 0, 0 , 0],
             ],
-        )
+        ),
+        
+        
+        (
+            [
+                [8, 4, 4 , 0],
+                [0, 0, 0 , 0],
+                [0, 0, 0 , 0],
+                [0, 0, 0 , 0],
+            ], 
+            Move.Left,
+            [
+                [8, 8, 0 , 0],
+                [0, 0, 0 , 0],
+                [0, 0, 0 , 0],
+                [0, 0, 0 , 0],
+            ],
+        ),
+    ]   
 )
-def test_movement(table, move, excpected):
+def test_movement(table, move, expected):
     board = Board(size=4)
     board._table = table
     board.move(move=move)
-    assert board._table == excpected
-
+    assert board._table == expected
+    # assert True
